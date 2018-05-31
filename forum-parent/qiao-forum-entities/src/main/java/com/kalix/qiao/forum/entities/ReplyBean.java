a2c7025e9cd5418e479c40522c7b3774b3cda085
yangze8818 <yangze_lmkj@163.com>
2018-05-31 08:58:09 +0800

    修改字段

--- forum-parent/qiao-forum-entities/src/main/java/com/kalix/qiao/forum/entities/ReplyBean.java
+++ forum-parent/qiao-forum-entities/src/main/java/com/kalix/qiao/forum/entities/ReplyBean.java
@@ -12,10 +12,10 @@ import javax.persistence.Table;
 @Table(name = "qiao_forum_reply")
 public class ReplyBean extends PersistentEntity {
     private String username; //回复人姓名
-    private int userid;      //排序
+    private int userId;      //排序
     private String content;  //回复内容
-    private int parentid;    //父节点
-    private int postid;      //发帖id
+    private int parentId;    //父节点
+    private int postId;      //发帖id
     private String category; //审核标识
 
     public String getUsername() {
@@ -26,36 +26,12 @@ public class ReplyBean extends PersistentEntity {
         this.username = username;
     }
 
-    public int getUserid() {
-        return userid;
-    }
-
-    public void setUserid(int userid) {
-        this.userid = userid;
-    }
-
     public String getContent() {
         return content;
     }
 
     public void setContent(String content) {
         this.content = content;
-    }
-
-    public int getParentid() {
-        return parentid;
-    }
-
-    public void setParentid(int parentid) {
-        this.parentid = parentid;
-    }
-
-    public int getPostid() {
-        return postid;
-    }
-
-    public void setPostid(int postid) {
-        this.postid = postid;
     }
 
     public String getCategory() {
@@ -66,4 +42,27 @@ public class ReplyBean extends PersistentEntity {
         this.category = category;
     }
 
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
 }
